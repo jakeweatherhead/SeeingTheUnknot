6cd16216fa24184c66766ba7ff1159594d51259d
jakeweatherhead <jakeweatherhead1@gmail.com>
2025-10-29 18:15:20 +0200

    Add run command

--- knot_data/pd/pd.py
+++ knot_data/pd/pd.py
@@ -15,7 +15,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
-# To run: $ ./run_sage pd.py
+# To run: $ ./run_sage.sh pd.py
 
 import os
 import json
