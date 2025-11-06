fcff83151647a8f668d00940c505a30c26adddf7
jakeweatherhead <jakeweatherhead1@gmail.com>
2025-11-06 20:42:23 +0200

    Add docstring to explain purpose of module

--- src/seeing-the-unknot/utils/utils.py
+++ src/seeing-the-unknot/utils/utils.py
@@ -15,6 +15,8 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
+"""General utility functions used throughout the pipeline."""
+
 import json
 import torch
 import random
